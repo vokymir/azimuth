ab676e9d79660c7ffde2a84abca97d5b6e82d1a1
Voky <voky.consultant@gmail.com>
2024-06-03 19:44:08 +0200

    test push

--- flask_app.py
+++ flask_app.py
@@ -111,13 +111,19 @@ def make_valid_filename(string: str) -> str:
 
     return newstr
 
+
 import git
-@app.route('/update_server', methods=['POST'])
+
+
+@app.route("/update_server", methods=["POST"])
 def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
+    if request.method == "POST":
+        repo = git.Repo("path/to/git_repo")
         origin = repo.remotes.origin
         origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
+        return "Updated PythonAnywhere successfully", 200
     else:
-        return 'Wrong event type', 400
+        return "Wrong event type", 400
+
+
+# test push

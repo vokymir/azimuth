80d82fd63c871e16904c3b819fd615ecc8ec4c01
Voky <voky.consultant@gmail.com>
2024-06-03 21:00:01 +0200

    securing

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, send_from_directory, render_template, Response
 import azimuth
-import git
+import os
 
 ALLOWED_EXTENSIONS = {"gpx"}
 
 app = Flask(__name__)
+w_secret = os.environ["SECRET_KEY"]
 
 
 def allowed_file(filename):
@@ -49,6 +50,11 @@ and now it auto-reloads after push
 @app.route("/update_server", methods=["POST"])
 def webhook():
     if request.method == "POST":
+        x_hub_signature = request.headers.get("X-Hub-Signature")
+        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+            return "Wrong signature", 400
+        import git
+
         repo = git.Repo("/home/vokymir/mysite")
         origin = repo.remotes.origin
         origin.pull()
@@ -153,3 +159,16 @@ def make_valid_filename(string: str) -> str:
             newstr += char
 
     return newstr
+
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    import hmac
+    import hashlib
+
+    hash_algorithm, github_signature = x_hub_signature.split("=", 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, "latin-1")
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)

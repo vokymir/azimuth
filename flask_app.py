ae171fb7c1b93e6d32c30e576a33a7c5f613f9eb
Voky <voky.consultant@gmail.com>
2024-06-03 19:37:56 +0200

    connect to pythonanywhere

--- flask_app.py
+++ flask_app.py
@@ -110,3 +110,14 @@ def make_valid_filename(string: str) -> str:
             newstr += char
 
     return newstr
+
+import git
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
